d67169537133dfc589dc33f807831d5284072553
DELL <ij290704@gmail.com>
2024-09-09 15:57:07 +0700

    hi

--- src/main/java/com/example/projectweb/Controller/QuanLy.java
+++ src/main/java/com/example/projectweb/Controller/QuanLy.java
@@ -1,6 +1,7 @@
 package com.example.projectweb.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,4 +10,9 @@ public class QuanLy {
 public String tranglsp(){
     return "quanlysanpham";
     }
+    @GetMapping("/trangchu5")
+    public String tranglsp(Model model){
+        return "/htmlcss/trangchu";
+    }
 }
+
